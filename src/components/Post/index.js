2fbc4a46158e13fcf59332f6403a9e9a93f592de
gualves <gustavo.allvess@icloud.com>
2024-03-01 00:00:09 -0300

    criando faviconnn

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,45 +1,66 @@
-import { useContext, useState, useEffect } from 'react'
-import { PostsContext } from '../../contexts/post' // Import the context itself
-import { Link } from 'react-router-dom'
-import { format } from 'date-fns'
+import { useEffect, useState } from 'react'
 
+import { format } from 'date-fns'
 import { Loader2 } from 'lucide-react'
 
-export default function Post({ toggleModal }) {
-  const { post = [] } = useContext(PostsContext)
+import { useNavigate, Link } from 'react-router-dom'
 
-  // Removed useMemo hook
+const baseURL = 'https://www.tabnews.com.br/api/v1'
+const endpoint = '/contents/GustAlves'
+
+export default function Post() {
+  const [post, setPost] = useState([])
+  const [load, setLoad] = useState(true)
+
+  const [modal, setModal] = useState(false)
+  const [detail, setDetail] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      if (post.length == 0) {
+        const response = await fetch(`${baseURL}${endpoint}`)
+        const data = await response.json()
+
+        setPost(data)
+        setLoad(false)
+      }
+    })()
+  }, [post])
+
+  if (load) {
+    return (
+      <div className="w-[500px] h-full flex items-center justify-center">
+        <Loader2 color="#fff" size={60} className="animate-spin" />
+      </div>
+    )
+  }
+
+  function toggleModal(post) {
+    setModal(!modal)
+    setDetail(post)
+  }
 
   return (
-    <div>
-      {post.length > 0 ? (
-        post.map(post => (
-          <Link key={post.id} to={`/${post.slug}`}>
+    <>
+      <div className="mt-5 mb-3 dark:text-slate-200 font-semibold text-slate-800  w-[350px] p-3 rounded-2xl text-center">
+        <h2>Confira abaixo ✍️ </h2>
+        {post.map(post => (
+          <Link to={`/${post.slug}`} key={post.id}>
             <button
+              onClick={() => toggleModal(post)}
+              className="border dark:border-white/35 border-black/20 dark:text-slate-200 font-semibold text-slate-800 text-left p-4 mt-3 rounded-2xl"
               key={post.id}
-              className="border w-[350px]  flex items-center flex-col dark:border-white/35 border-black/20 dark:text-slate-200 font-semibold text-slate-800 text-left p-4 mt-10 rounded-2xl"
             >
-              <img
-                className="rounded-lg mb-2"
-                src={post.source_url}
-                alt={post.title}
-              />
+              <img className="rounded-lg mb-2" src={post.source_url} />
               <h2>{post.title}</h2>
               <div className="flex flex-row items-center justify-between m-2 text-xs opacity-80">
                 <p>Escrito por: {post.owner_username}</p>
-                <p>{format(post?.created_at, 'dd/MM/yyyy')}</p>
+                <p>{format(post?.created_at, 'dd.MM.yyy')}</p>
               </div>
             </button>
           </Link>
-        ))
-      ) : (
-        <div className="w-[500px] flex items-center justify-center h-[100vh] bg-slate-200 dark:bg-slate-900 ">
-          <Loader2
-            size={60}
-            className="animate-spin dark:text-slate-200 text-slate-800"
-          />
-        </div>
-      )}
-    </div>
+        ))}
+      </div>
+    </>
   )
 }
